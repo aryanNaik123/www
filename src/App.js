381977c742ed84e5d5b6161eb23183400da6e4a9
Aryan <aryannnaik@outlook.com>
2021-12-07 22:03:54 -0500

    centering divs and stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Header">Aryan Naik</div>
+      <div style={{ display: "flex" }}>
+        <ul style={{ margin: "0 auto" }}>
+          <li>Builder</li>
+          <li>Data Scientist</li>
+          <li>Researcher</li>
+          <li>Explorer</li>
+        </ul>
+      </div>
+      <div
+        style={{ paddingTop: "100px", paddingBottom: "100px", display: "flex" }}
+      >
+        <div style={{ margin: "0 auto" }}>
+          <h1
+            style={{
+              textAlign: "center",
+              Width: "600px",
+              float: "left",
+              marginRight: "100px",
+            }}
+          >
+            Working on...
+          </h1>
+          <ul style={{ margin: "0 auto" }}>
+            <li>Hack^2: HTNE + Hack for The People</li>
+            <li>Harvard Erevna CVT</li>
+            <li>Democratizing Data Science</li>
+            <li>Exploring Web3</li>
+          </ul>
+        </div>
+      </div>
+      <div style={{ display: "flex" }}>
+        <h1 style={{ margin: "0 auto", textDecoration: "underline" }}>
+          Skills
+        </h1>
+      </div>
+    </>
   );
 }
 
